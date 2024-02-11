d1b33c09462173a3760eff2100d39f29bf1789e6
Benjamin Daly <56139197+mojius@users.noreply.github.com>
2024-02-11 14:07:37 -0800

    First refactor of CursorComp

--- Scripts/CursorComp.gd
+++ Scripts/CursorComp.gd
@@ -5,10 +5,10 @@ var Bspwn = preload("res://Objects/Enemy.tscn")
 @export var Offset = Vector2(8, 8)
 @onready var parent = get_parent()
 @onready var root = get_tree().root.get_child(0)
-@onready var mc = root.get_node("MenuCombat")
+@onready var menu_combat = root.get_node("MenuCombat")
 @onready var sprt : AnimatedSprite2D = get_node("../AnimatedSprite2D")
-var ID = -1
-var SID = -1
+@onready var ID = root.DEFAULT_ID
+@onready var selected_id = root.DEFAULT_ID
 @export var SpriteID = 0
 var inited = false
 
@@ -17,6 +17,7 @@ func _ready():
 	await parent.ready
 	await root.ready
 	ID = parent.ID
+	sprt.frame = 1
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
@@ -26,59 +27,85 @@ func _process(delta):
 		inited = true
 
 func _input(event):
-	if(!(event is InputEventMouseMotion)):
-		var input_direction = Input.get_vector("left", "right", "up", "down")
-		input_direction = input_direction.ceil()
-		if(input_direction != Vector2(0, 0)):
-			print(input_direction)
-		input_direction = Vector2i(input_direction)
-		root.ID_manager.location[ID] += input_direction
-		root.ID_manager.location[ID] = root.ID_manager.location[ID].clamp(Vector2i(0,0),root.tile_map.get_used_rect().size)
-		parent.position = Vector2(root.ID_manager.location[ID]*16) + Offset
-		print("my ID is: ", parent.ID)
-	var TSID = root.ID_manager.get_id(root.ID_manager.location[ID], ID)
-	print("at ", root.ID_manager.location[ID], " is the ID: ",TSID)
-	if(TSID == -1):
-		sprt.frame = 0
-	else:
-		sprt.frame = 1
+
+	var hovered_over_id = root.ID_manager.get_id(root.ID_manager.location[ID], ID)
+
+	if((event is InputEventKey)):
+		shift_cursor(hovered_over_id)
 	
-	if(event.is_action_pressed("select")):
-		if(root.faction_manager.player_turn and !root.move_manager.moving):
-			print("My ID:", ID)
-			print("Selected ID:",SID)
-			print("Under ID:",TSID)
-			if(TSID == -1):
-				if(SID != -1):
-					if(root.move_manager.actor_move(SID, root.ID_manager.location[ID])):
-						SID = -1
-						root.combat_display_manager.remove_indicator()
-						mc.visible = false
-			elif(SID == -1):
-				var TSID_object_dynamic = root.ID_manager.id_to_obj[TSID].find_child("C_Dynamic")
-				if(root.ID_manager.id_to_obj[TSID].selectable and !TSID_object_dynamic.moved):
-					SID = TSID
-					root.ID_manager.id_to_obj[SID].clicked()
-					mc.visible = true
-			elif(SID == TSID):
-				SID = -1
-				root.combat_display_manager.remove_indicator()
-				mc.visible = false
-			else:
-				print("INTER")
-				root.combat_manager.actor_deal_damage(SID, TSID)
-				mc.visible = false
-		
-	if(event.is_action_pressed("db_Spawn")):
-		print("Spawning")
-		var OBJ = spwn.instantiate()
-		OBJ.Init_pos = root.ID_manager.location[ID]
-		root.add_child(OBJ)
-		root.faction_manager.get_move(root.faction_manager.player_faction)
-		
+	if(event.is_action_pressed("select")
+		and root.faction_manager.player_turn 
+		and !root.move_manager.moving):
+
+		# First case: If under the cursor is a blank selection, but the cursor has a unique selection, move the actor if possible
+		if(hovered_over_id == root.DEFAULT_ID and selected_id != root.DEFAULT_ID):
+			check_can_move_actor()
+			return
+
+		# Second case: If the cursor has no selection, or has the same selection, see if you can select/deselect.
+		if (check_can_select_deselect_object(hovered_over_id)):
+			return
+
+		# Third case: If your selected id is different from your hovered id, try attacking that thing i guess.
+		else:
+			print("Attack granted!")
+			root.combat_manager.actor_deal_damage(selected_id, hovered_over_id)
+			menu_combat.visible = false
+
+		check_spawn_player(event)
+		check_spawn_enemy(event)
+
+
+func check_can_select_deselect_object(hovered_over_id):
+	if(selected_id == root.DEFAULT_ID):
+		var hovered_over_id_object_dynamic = root.ID_manager.id_to_obj[hovered_over_id].find_child("C_Dynamic")
+		if(root.ID_manager.id_to_obj[hovered_over_id].selectable and !hovered_over_id_object_dynamic.moved):
+			selected_id = hovered_over_id
+			root.ID_manager.id_to_obj[selected_id].clicked()
+			menu_combat.visible = true
+			return true
+
+	# If the cursor is hovering over the selected object, deselect it.
+	elif(selected_id == hovered_over_id):
+		selected_id = root.DEFAULT_ID
+		root.combat_display_manager.remove_indicator()
+		menu_combat.visible = false
+		return true
+
+	return false
+
+	
+func check_can_move_actor():
+	if(root.move_manager.actor_move(selected_id, root.ID_manager.location[ID])):
+		selected_id = root.DEFAULT_ID
+		root.combat_display_manager.remove_indicator()
+		menu_combat.visible = false
+
+
+func shift_cursor(hovered_over_id):
+
+	print("at ", root.ID_manager.location[ID], " is the ID: ",hovered_over_id)
+
+	var input_direction = Input.get_vector("left", "right", "up", "down")
+	input_direction = input_direction.ceil()
+
+	input_direction = Vector2i(input_direction)
+	root.ID_manager.location[ID] += input_direction
+	root.ID_manager.location[ID] = root.ID_manager.location[ID].clamp(Vector2i(0,0),root.tile_map.get_used_rect().size)
+
+	parent.position = Vector2(root.ID_manager.location[ID]*16) + Offset
+
+func check_spawn_enemy(event):
 	if(event.is_action_pressed("db_Spawn_bad")):
 		print("Spawning baddie")
 		var OBJ = Bspwn.instantiate()
 		OBJ.Init_pos = root.ID_manager.location[ID]
 		root.add_child(OBJ)
 
+func check_spawn_player(event):
+	if(event.is_action_pressed("db_Spawn")):
+		print("Spawning")
+		var OBJ = spwn.instantiate()
+		OBJ.Init_pos = root.ID_manager.location[ID]
+		root.add_child(OBJ)
+		root.faction_manager.get_move(root.faction_manager.player_faction)
