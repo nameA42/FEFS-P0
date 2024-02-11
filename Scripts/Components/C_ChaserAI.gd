def698b85bfed5f7ac218f253d399cd28cfc00a9
bepratt <bepratt@ucsc.edu>
2024-02-10 20:08:58 -0800

    fixed small bug with damage

--- Scripts/Components/C_ChaserAI.gd
+++ Scripts/Components/C_ChaserAI.gd
@@ -22,6 +22,6 @@ func _process(delta):
 func Act():
 	var closest_ID = root.faction_manager.get_closest_faction_member(root.faction_manager.player_faction, root.ID_manager.location[ID])
 	if(root.combat_display_manager.get_in_range_area(ID, Stats.rang).has(root.ID_manager.location[closest_ID])):
-		root.combat_manager.actor_move(ID, closest_ID)
+		root.combat_manager.actor_deal_damage(ID, closest_ID)
 	elif(!parent.get_node("C_Dynamic").moved):
 		root.move_manager.actor_move(ID, root.ID_manager.location[closest_ID], Stats.speed)
